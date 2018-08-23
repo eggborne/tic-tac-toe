261afb1d56b12e3052e7b1e5fc42139e80137203
Michael Donovan <mike@eggborne.com>
2018-08-22 21:01:40 -0700

    Add Player and Board classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,73 @@
-var board = [];
+var boardContents = [];
+var players = [];
+var symbols = ["x","o"]
 var currentPlayer;
 $(function(){
-  CG = new ColumnGenerator();
-  CG.insertLayout('board',boardLayout,'#board-area',true)
-  makeSpacesSquare()
-  var totalSpaces = boardLayout.length*(boardLayout[0][0])
-  boardLayout.forEach(function(row,i){
-    var row = row[0]
-    for (var j=0; j<row.length; j++) {
-      new Space(i,j)
-    }
-  })
+  generator = new ColumnGenerator();
+  board = new Board(boardLayout)
+  player1 = new Player()
+  player2 = new Player()
+  currentPlayer = player1
 })
-function Space(row,column) {
+function Space(board,row,column) {
+  console.log(board + " board");
+  
   this.div = $('#column-'+row+'-'+column)
   this.symbol = "blank"
+  this.board = board
   this.row = row
   this.column = column
-  this.changeSymbol = function(newSymbol) {
-    this.symbol = newSymbol
-    this.div.removeClass('x')
-    this.div.removeClass('o')
-    this.div.addClass(this.symbol)
-  }
   var self = this
   this.div.mousedown(function(event){
     if (event.button === 0) {
-      self.changeSymbol('x')
+      self.changeSymbol(currentPlayer.symbol)
     } else {
       self.changeSymbol('blank')
     }
   })
-  if (!board[row]) {
-    board[row] = []
+  if (!this.board.contents[row]) {
+    this.board.contents[row] = []
   }
-  board[row][column] = this
-  this.changeSymbol('o')
+  this.board.contents[row][column] = this
+  this.changeSymbol('blank')
+}
+Space.prototype.changeSymbol = function(newSymbol) {
+  this.symbol = newSymbol
+  this.div.removeClass('x')
+  this.div.removeClass('o')
+  this.div.addClass(this.symbol)
+}
+function Player(name) {
+  this.index = players.length
+  this.symbol = symbols[this.index]
+  this.name = name
+  players.push(this)
+}
+function Board(layout) {
+  this.div = $('#board')
+  this.layout = layout
+  this.contents = []
+  this.checkForLine = function() {
+    
+  }
+  generator.insertLayout('board',boardLayout,'#board-area',true)
+  this.layout.forEach(function(row,i){
+    var row = row[0]
+    for (var j=0; j<row.length; j++) {
+      new Space(this,i,j)
+    }
+  },this)
+  makeSpacesSquare()
 }
 var boardLayout = [
   [[4,4,4]],
   [[4,4,4]],
   [[4,4,4]]
 ]
+// responsiveness
 function makeSpacesSquare() {
   var spaceSize = $('#row-0').width()/3
-  // var spaceSize = ($('.container').width()/2)/3 // 1/3 of a col-6
-  CG.changeRowHeight("board","all",spaceSize+"px")
+  generator.changeRowHeight("board","all",spaceSize+"px")
 }
 window.addEventListener('resize',function(){
   if ($('#board').height() !== $('#board').width()) {
