ba52448d19a9cdadb50cd82cf57f576090aaa4c0
Michael Donovan <mike@eggborne.com>
2018-08-22 18:03:08 -0700

    Add listener to normalize board shape on resize

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(function(){
-  new ColumnGenerator().insertLayout('board',boardLayout,'#board-area',true)
+  CG = new ColumnGenerator();
+  CG.insertLayout('board',boardLayout,'#board-area',true)
+  makeSpacesSquare()
   $('.column.generated').click(function(){
     console.log("clicked " + this.id)
     $(this).css({
@@ -8,7 +10,18 @@ $(function(){
   })
 })
 var boardLayout = [
-  [[4,4,4],'120px'],
-  [[4,4,4],'120px'],
-  [[4,4,4],'120px']
+  [[4,4,4]],
+  [[4,4,4]],
+  [[4,4,4]]
 ]
+function makeSpacesSquare() {
+  console.log("squaring...")
+  var spaceSize = ($('.container').width()/2)/3 // 1/3 of a col-6
+  CG.changeRowHeight("board","all",spaceSize+"px")
+}
+window.addEventListener('resize',function(){
+  console.log($('#board').width())
+  if ($('#board').height() !== $('#board').width()) {
+    makeSpacesSquare()
+  }
+})
